8e74685e14ebbfe14850ea17388089b8721d4de7
Fernando Crespo <fernando82@gmail.com>
2022-09-24 18:20:19 -0300

    Add function to edit file in choice editor or default one Change profile setting to -s --set Set/uset commit.gpgsign per profile

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ from argparse import ArgumentParser
 python_script_dir = os.path.dirname(os.path.realpath(__file__))
 
 config = configparser.ConfigParser()
-config.read(os.path.join(python_script_dir, 'config.ini'))
+CONFIG_PATH = os.path.join(python_script_dir, 'config.ini')
+TEMPLATE_CONFIG_PATH = os.path.join(python_script_dir, 'config.ini.template')
+config.read(CONFIG_PATH)
 
 
 def setup_parser():
-    parser = ArgumentParser()
-    parser.add_argument('profile', type=str, choices=config.sections())
-    return parser.parse_args()
+    arg_parser = ArgumentParser()
+    group = arg_parser.add_mutually_exclusive_group()
+    group.add_argument('-s', '--set', type=str,
+                       choices=config.sections(), required=False,
+                       help='Set the git author to be one of the configured profiles')
+    group.add_argument('-e', '--edit', action='store_true', required=False,
+                       help='Open a text editor to configure the profiles')
+    return arg_parser
+
+
+def is_git():
+    return subprocess.run(['git', 'status'], check=False,
+                          capture_output=True).returncode == 0
 
 
 def setup_git(profile):
@@ -24,27 +36,60 @@ def setup_git(profile):
                     'user.name', config[profile]['name']]
     command_email = ['git', 'config', '--local',
                      'user.email', config[profile]['email']]
-    command_signing_key = None
+    command_signing_key = []
+    command_sign_commit = []
     if 'signingkey' in config[profile]:
         command_signing_key = ['git', 'config', '--local',
                                'user.signingkey', config[profile]['signingkey']]
+        command_sign_commit = ['git', 'config',
+                               '--local', 'commit.gpgsign', 'true']
     command_signing_key_unset = ['git', 'config',
                                  '--local', '--unset', 'user.signingkey']
-
-    is_git = subprocess.run(['git', 'status'], check=False,
-                            capture_output=True).returncode == 0
-
-    if is_git:
+    command_sign_commit_unset = ['git', 'config',
+                                 '--local', '--unset', 'commit.gpgsign']
+    if is_git():
         subprocess.run(command_name, check=False)
         subprocess.run(command_email, check=False)
         if command_signing_key:
             subprocess.run(command_signing_key, check=False)
+            subprocess.run(command_sign_commit, check=False)
         else:
             subprocess.run(command_signing_key_unset, check=False)
+            subprocess.run(command_sign_commit_unset, check=False)
     else:
         print('Not a git repository')
 
 
+def edit_file():
+    if not os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, 'w', encoding='UTF-8') as empty_config:
+            with open(TEMPLATE_CONFIG_PATH, 'r', encoding='UTF-8') as template_config:
+                empty_config.writelines(template_config.readlines())
+
+    import sys
+    platform = sys.platform
+    editor = 'vim'
+    if platform == 'linux':
+        editor = 'vim'
+    elif platform == 'win32':
+        editor = 'notepad.exe'
+
+    if 'EDITOR' in os.environ:
+        editor = os.environ['EDITOR']
+
+    subprocess.run(editor.split(' ') + [CONFIG_PATH], check=True)
+
+
 if __name__ == '__main__':
-    args = setup_parser()
-    setup_git(args.profile)
+    if not is_git():
+        print('You need to run this command inside a git repository')
+        exit(1)
+    parser = setup_parser()
+    args = parser.parse_args()
+    if args.edit:
+        edit_file()
+        exit(2)
+    elif args.set:
+        setup_git(args.set)
+    else:
+        parser.print_help()
