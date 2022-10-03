d2e8627d21f01d5b397e3571ecc1435c6fa6c76e
Fernando Crespo <fernando82@gmail.com>
2022-10-03 15:09:23 -0300

    Install compatible with mac

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir -p $HOME/.local/bin
 
-ln -sf `realpath ./main.py` $HOME/.local/bin/git-author
+ln -sf "$(pwd)/main.py" "$HOME/.local/bin/git-author"
 
 echo "Please make sure there's a config.ini in this folder with your required profiles"
