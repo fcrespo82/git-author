03bdb3800ebb586244a2a30f5e547c1ef947a43f
Fernando Crespo <fernando82@gmail.com>
2023-03-08 15:16:56 -0300

    Move to src-layout and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="git-author",
-    version="1.0.0",
-    author="Fernando Crespo",
-    author_email="fernando82@gmail.com",
-    packages=[],
-    entry_points="""
-    [console_scripts]
-    git-author=git_author:cli
-    """,
-    install_requires=["click", "tabulate"],
-    include_package_data=True
-)
+setup()
